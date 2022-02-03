b03bb324a48ec0c379cd1afd5ad16f7aedf6a1ac
Bogdan Kalnybolotchuk <bogdan.kalnybolotchuk@gmail.com>
2022-02-03 12:14:03 +0200

    add new

---
+++ lists-methods/main.py
@@ -0,0 +1,18 @@
+# some_list = [12, "Hello", 15.5, 11]
+# print(some_list)
+# some_list.append("Bogdan")
+# print(some_list)
+# print(some_list.pop(0))
+# print(some_list)
+
+#some_list.insert(2, "2")
+# new_list = [11, 22, 33]
+# some_list.extend(new_list)
+# some_list.append(new_list)
+# print(some_list)
+# print(some_list[7])
+
+x = [1, 2, 3]
+y = [11, 22, 33]
+y.extend(x)
+print(y)

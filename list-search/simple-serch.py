7cee7674df533c66a763ec9a57fe5f14b0f27c8e
Bogdan Kalnybolotchuk <bogdan.kalnybolotchuk@gmail.com>
2022-02-10 11:52:26 +0200

    add fs

---
+++ list-search/simple-serch.py
@@ -0,0 +1,7 @@
+number_list = [11, 12, 14, 3, 7, 13, 14, 11, 8, 9]
+searched_element = 15
+for el in number_list:
+    if el == searched_element:
+        print()
+    else:
+        print("Ні, елементу в списку немає")
